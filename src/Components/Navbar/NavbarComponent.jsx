71a212dd7515597062c4595767fd065ece09d0a8
Shust0vD <99994722+Shust0vD@users.noreply.github.com>
2022-07-29 00:54:25 +0300

    fix: добавил SVG компоненты в Navbar

--- src/Components/Navbar/NavbarComponent.jsx
+++ src/Components/Navbar/NavbarComponent.jsx
@@ -2,18 +2,25 @@ import { Nav, Container, Navbar } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
+import ActivitySVG from '../SVG/ActivitySVG';
+import MapSVG from '../SVG/MapSVG';
+import TimeSVG from '../SVG/TimeSVG';
+
 export default function NavbarComponent() {
   return (
     <Navbar bg="light" variant="light" className="navbar">
       <Container>
         <Nav>
           <NavLink to="/" className={({ isActive }) => (isActive ? 'active_link' : 'inactive_link')}>
+            <ActivitySVG />
             <p className="activity_title">Activity</p>
           </NavLink>
           <NavLink to="/map" className={({ isActive }) => (isActive ? 'active_link' : 'inactive_link')}>
+            <MapSVG />
             <p className="map_title">Map</p>
           </NavLink>
           <NavLink to="/time" className={({ isActive }) => (isActive ? 'active_link' : 'inactive_link')}>
+            <TimeSVG />
             <p className="time_title">Time</p>
           </NavLink>
         </Nav>
